abc09a75c02d079c00437203f4bda89eb761526a
Vini <benvini8@gmail.com>
2021-05-14 06:10:59 +0300

    add form validation with yup

--- ts/screens/GameOverScreen/GameOverScreen.tsx
+++ ts/screens/GameOverScreen/GameOverScreen.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
-import {get} from 'lodash';
+import { get } from 'lodash';
 import styled from 'styled-components/native';
 import { Formik } from 'formik'
-import {Input} from 'react-native-elements'; 
+import { Input } from 'react-native-elements';
+import * as yup from 'yup'
 
 import { Screen, Typography } from '../../shared/components';
 import COLOR from '../../styles/Color';
@@ -11,6 +12,7 @@ import MainButton from '../../shared/components/MainButton';
 
 const StyledInput = styled.View`
     width: 200px;
+    margin-top: 12px;
 `
 
 const FormContainer = styled.View`
@@ -24,6 +26,19 @@ const FormContainer = styled.View`
     margin-bottom: 16px;
 `
 
+const formValidationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, ({ min }) => `Name must be at least ${min} characters`)
+    .required('Name is Required'),
+  phone: yup
+    .number()
+    .positive()
+    .integer()
+    .test('len', 'Phone must be exactly 9 characters', (val) => val?.toString().length === 9)
+    .required('Phone is required'),
+});
+
 const GameOverScreen: FunctionComponent = () => {
   const [score, setScore] = useState(4);
   const route = useRoute();
@@ -32,7 +47,7 @@ const GameOverScreen: FunctionComponent = () => {
   useEffect(() => {
     const userScore = get(route.params, 'score', -1);
     setScore(userScore)
-    
+
     if (userScore === -1) {
       console.error('Bad input. Could not get score.')
     }
@@ -45,42 +60,49 @@ const GameOverScreen: FunctionComponent = () => {
   const onMainMenu = useCallback(() => {
     navigation.navigate('Main Menu')
   }, []);
-  
+
   return (
     <Screen>
-      <Typography style={{margin: 4}}>Your Score: {score}</Typography>
-      <Typography style={{margin: 4}}>High Score:</Typography>
+      <Typography style={{ margin: 4 }}>Your Score: {score}</Typography>
+      <Typography style={{ margin: 4 }}>High Score:</Typography>
       <FormContainer>
-      <Formik
-            initialValues={{ name: '', phone: '' }}
-            onSubmit={values => console.log(values)}
-          >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <>
-              <Typography style={{fontWeight: 'bold', marginBottom: 14, fontSize: 24, color: COLOR.PRIMARY}}>Submit your score</Typography>
-                <StyledInput><Input
-                  key="name"
-                  placeholder="Name"
-                  onChangeText={handleChange('name')}
-                  onBlur={handleBlur('name')}
-                  autoFocus
-                  value={values.name}
-                />
-                </StyledInput>
-                <StyledInput><Input
-                  key="phone"
-                  placeholder="Phone"
-                  keyboardType="numeric"
-                  onChangeText={handleChange('phone')}
-                  onBlur={handleBlur('phone')}
-                  value={values.phone}
-                />
-                </StyledInput>
-                <MainButton onPress={handleSubmit} title="Submit" style={{backgroundColor: COLOR.SUCCESS}}/>
-              </>
-            )}
+        <Formik
+          validationSchema={formValidationSchema}
+          initialValues={{ name: '', phone: '' }}
+          onSubmit={values => console.log(values)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+            <>
+              <Typography style={{ fontWeight: 'bold', fontSize: 24, color: COLOR.PRIMARY }}>Submit your score</Typography>
+              <StyledInput><Input
+                key="name"
+                placeholder="Name"
+                onChangeText={handleChange('name')}
+                onBlur={handleBlur('name')}
+                autoFocus
+                value={values.name}
+              />
+              </StyledInput>
+              {errors.name &&
+                <Typography style={{ fontSize: 10, color: 'red' }}>{errors.name}</Typography>
+              }
+              <StyledInput><Input
+                key="phone"
+                placeholder="Phone"
+                keyboardType="numeric"
+                onChangeText={handleChange('phone')}
+                onBlur={handleBlur('phone')}
+                value={values.phone}
+              />
+              </StyledInput>
+              {errors.phone &&
+                <Typography style={{ fontSize: 10, color: 'red' }}>{errors.phone}</Typography>
+              }
+              <MainButton onPress={handleSubmit} title="Submit" disabled={!isValid} style={{ backgroundColor: COLOR.SUCCESS, marginTop: 12 }} />
+            </>
+          )}
         </Formik>
-        </FormContainer>
+      </FormContainer>
       <MainButton title="Leaderboards" style={{ width: 140 }}></MainButton>
       <MainButton title="New Game" onPress={onNewGame}></MainButton>
       <MainButton title="Main Menu" onPress={onMainMenu}></MainButton>
